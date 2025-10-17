3e55b8fde9b2f2824346c794ebc088a806b0c9b7
Richardeze <ezerichard100@gmail.com>
2025-10-17 20:26:16 +0100

    HNG13 Stage 0 Task completed

---
+++ README.md
@@ -0,0 +1,13 @@
+# HNG 13 Backend Wizards - Stage ) Task
+
+## Description
+This project is a simple Flask RESTful API that returns my profile information along with a dynamic cat fact fetched 
+from an external API.
+
+Project goals
+- Building a simple REST API
+- Consuming third party APIs with the current timestamp in ISO 8601 format
+- Fetching random cat fact from the Cat Facts API
+- Returning dynamic json data
+
+## Setup Instructions
---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+import requests
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False
+
+CAT_API = "https://catfact.ninja/fact"
+
+@app.route("/me", methods=["GET"])
+def get_my_profile():
+    """"
+    Returns my profile details and a random cat fact
+    """
+    try:
+        response = requests.get(url=CAT_API, timeout=5)
+        response.raise_for_status()
+        cat_fact = response.json().get("fact", "No facts returned")
+
+    except requests.RequestException:
+        cat_fact = "Could not fetch cat fact at the moment. Try again later."
+
+    my_data = {
+        "status": "success",
+        "user": {
+            "email": os.environ["USER_EMAIL"],
+            "name": os.environ["USER_NAME"],
+            "stack":os.environ["USER_STACK"]
+        },
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "fact": cat_fact
+    }
+
+    return  jsonify(my_data), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
