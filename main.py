ac53c53cf93865d3272b775183ed49015ba865be
Richardeze <ezerichard100@gmail.com>
2025-10-18 03:36:08 +0100

    Configured Gunicorn for railway deployment

--- main.py
+++ main.py
@@ -42,6 +42,5 @@ def get_my_profile():
     return  jsonify(my_data), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run()
 
